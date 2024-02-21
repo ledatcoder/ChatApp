8a4f9a86330b15bec880cc65d13134468cfcd6db
Lê Thành Đạt <datthichlaptrinh@gmail.com>
2024-02-21 22:58:37 +0700

    Work All Layout

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
-const { app, BrowserWindow, Notification } = require('electron');
-
+const { app, BrowserWindow, Notification } = require("electron");
+const path = require("path");
+const isDev = !app.isPackaged;
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-    win.loadFile('index.html')
-    win.webContents.openDevTools();
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+    //   worldSafeExecuteJavascript: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    },
+  });
+  win.loadFile("index.html");
+  isDev && win.webContents.openDevTools();
 }
 
-app.whenReady()
-  .then(() => {
-    createWindow();
-    const notification = new Notification({title: 'Hello World', body: 'My test message'});
-    notification.show();
+if (isDev) {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron"),
   });
+}
 
-app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin'){
-        app.quit();
-    }
-})
+app.whenReady().then(createWindow);
 
-app.on('activate', () => {
-    if(BrowserWindow.getAllWindows().length === 0){
-        createWindow();
-    }
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
