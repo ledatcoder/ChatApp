77b87b4f7b8cc1d282a20ecdafd6620dd5e6404e
Lê Thành Đạt <datthichlaptrinh@gmail.com>
2024-02-23 21:07:58 +0700

    FireBase Config

---
+++ src/js/components/shared/ViewTitle.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+
+export default function ViewTitle({text}) {
+
+  return (
+    <div className="chat-name-container">
+      <span className="name">{text}</span>
+      
+    </div>
+  )
+}
---
+++ src/js/views/Home.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import ViewTitle from "../components/shared/ViewTitle";
+import JoinedChatsList from "../components/JoinedChatsList";
+import AvailableChatsList from "../components/AvailableChatsList";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchChats } from "../actions/chats";
+export default function Home() {
+  const dispatch = useDispatch();
+  const chats = useSelector(({ chats }) => chats.items);
+  useEffect(() => {
+    dispatch(fetchChats());
+  }, [dispatch]);
+  return (
+    <div className="row no-gutters fh">
+      <div className="col-3 fh">
+        <JoinedChatsList chats={chats} />
+      </div>
+      <div className="col-9 fh">
+        <ViewTitle text="Choose your channel" />
+        <AvailableChatsList chats={chats} />
+      </div>
+    </div>
+  );
+}
